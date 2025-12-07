bee76c1626407a5b2ffe08a6019e1918e6a2130b
mdp-eu <macedoni.italia@gmail.com>
2025-12-08 00:01:18 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'ea-preventivi-v1';
+const URLS_TO_CACHE = [
+  './',
+  './preventivi.html',
+  './style.css',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
